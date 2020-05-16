b4c1a943d68cb38e170168c7d6165efd1dc2da68
Kevin Rodgers <kevjrodgers@gmail.com>
2020-05-16 15:29:15 -0700

    Work Timer Functionality

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 let TwentyFiveTimer = 25;
 let BreakTimer = 5;
-let minutes;
-let seconds;
+let count = 0;
+let seconds = 1500;
+let pause = true;
 
 const workButtonIncrease = document.getElementById("work-increase");
 const workButtonDecrease = document.getElementById("work-decrease");
 const breakButtonIncrease = document.getElementById("break-increase");
-const breakButtonIncrease = document.getElementById("break-increase");
-const pauseButton 
+const breakButtonDecrease = document.getElementById("break-decrease");
+const pauseButton = document.getElementById("pause-button");
+const startButton = document.getElementById("start-button");
+const displayTimer = document.getElementById("timer");
+
+// start
+startButton.addEventListener('click', () => {
+    clearInterval(count);
+    pause = !pause;
+    if(!pause){
+        count = setInterval(timer, 1000);
+    }
+})
+
+// timer function
+function timer() {
+    // countdown
+    seconds--;
+    countdown();
+    // when seconds reaches zero stop
+    if(seconds < 0) {
+        clearInterval(count);
+        pause = !pause;
+        count = setInterval(timer, 1000);
+    }
+}
+
+// HTML Functions
+function countdown() {
+    let minutes = Math.floor(seconds / 60);
+    let updateSeconds = Math.floor(seconds % 60);
+    
+    displayTimer.innerHTML = `${minutes}:${updateSeconds < 10 ? '0' : ""}${updateSeconds}`;
+}
