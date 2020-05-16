d42c746a9c4ddb984bdff11f8dc2a92f93250a1c
Kevin Rodgers <kevjrodgers@gmail.com>
2020-05-16 14:16:09 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let TwentyFiveTimer = 25;
+let BreakTimer = 5;
+let minutes;
+let seconds;
+
+const workButtonIncrease = document.getElementById("work-increase");
+const workButtonDecrease = document.getElementById("work-decrease");
+const breakButtonIncrease = document.getElementById("break-increase");
+const breakButtonIncrease = document.getElementById("break-increase");
+const pauseButton 
